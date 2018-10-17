56bbdbfeecce2fa8ff2046019bf7c017e03675e8
Spinnin <1315025826@qq.com>
2018-10-17 10:46:43 +0800

    qwer

---
+++ src/main/java/com/example/demo/GitHub1017Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHub1017Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitHub1017Application.class, args);
+	}
+}
